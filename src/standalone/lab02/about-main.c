ce7090c5b4d7d070a0c9a18de8c26ee18ebfb04e
Rayan Bayat <rayba120@student.liu.se>
2022-03-31 16:24:22 +0200

    Lab2 klar

--- src/standalone/lab02/about-main.c
+++ src/standalone/lab02/about-main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 int main(int argc, char* argv[])
 {
+int totallength = 0;
+for (int i = 0; i < argc ; ++i)
+  {
+    int spacebetween = 20 - strlen(argv[i]);
+    int numberofchar = strlen(argv[i]);
+    totallength += numberofchar;
+    printf("%s", argv[i]);
+    printf("%*d\n",spacebetween,numberofchar);
+  }
+  int spacebetween = 20 - strlen("Total length");
+  printf("Total length%*d\n",spacebetween,totallength);
+  spacebetween = 20 - strlen("Average length");
+  printf("Average length%*.2f\n",spacebetween,((float)totallength/(float)argc));
+  
+return totallength;
 }
+
+
